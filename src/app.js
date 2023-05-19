0378cf5e87913a39df223eb979727326e6c811b5
Pedro Luis Alvarado Herrera <129191234+pedroalvaradoh@users.noreply.github.com>
2023-05-19 20:38:17 +0000

    feat: Random Domain Name Generator Added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("domainId").innerHTML = domainNameArray;
+
+  var pronoun = ["the", "our", "your"];
+  var adj = ["great", "big", "modern", "powerful"];
+  var noun = ["jogger", "racoon"];
+  let domainNameArray = [];
+
+  function randomDomainGenerator(pronounP, adjP, nounP) {
+    for (var i = 0; i < pronounP.length; i++) {
+      for (var e = 0; e < adjP.length; e++) {
+        for (var u = 0; u < nounP.length; u++) {
+          let randomize = pronounP[i] + adjP[e] + nounP[u] + ".com";
+          domainNameArray.push(randomize);
+        }
+      }
+    }
+    return domainNameArray;
+  }
+  randomDomainGenerator(pronoun, adj, noun);
+  console.log(domainNameArray);
 };
