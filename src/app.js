a0b38a0989d92f147254149f890539cb940748e3
Pedro Luis Alvarado Herrera <129191234+pedroalvaradoh@users.noreply.github.com>
2023-05-20 01:37:21 +0000

    refactor: added styles to the main view

--- src/app.js
+++ src/app.js
@@ -6,24 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  document.getElementById("domainId").innerHTML = domainNameArray;
-
   var pronoun = ["the", "our", "your"];
   var adj = ["great", "big", "modern", "powerful"];
   var noun = ["jogger", "racoon"];
   let domainNameArray = [];
-
   function randomDomainGenerator(pronounP, adjP, nounP) {
     for (var i = 0; i < pronounP.length; i++) {
       for (var e = 0; e < adjP.length; e++) {
         for (var u = 0; u < nounP.length; u++) {
-          let randomize = pronounP[i] + adjP[e] + nounP[u] + ".com";
+          let randomize =
+            "<i>www.</i>" +
+            "<strong>" +
+            pronounP[i] +
+            adjP[e] +
+            nounP[u] +
+            "</strong>" +
+            "<i>.com</i>";
+
           domainNameArray.push(randomize);
         }
       }
     }
-    return domainNameArray;
+    return domainNameArray.join("<br>");
   }
-  randomDomainGenerator(pronoun, adj, noun);
-  console.log(domainNameArray);
+
+  document.getElementById("domainId").innerHTML = randomDomainGenerator(
+    pronoun,
+    adj,
+    noun
+  );
+
+  // console.log(randomDomainGenerator(pronoun, adj, noun));
 };
